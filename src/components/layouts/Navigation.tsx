71d3f87b52fd20bce1ad94339390c2e4ab8767f2
sigidhanafi <sigidhanafi@gmail.com>
2023-04-09 13:53:43 +0700

    add navigation back

--- src/components/layouts/Navigation.tsx
+++ src/components/layouts/Navigation.tsx
@@ -6,7 +6,28 @@ const Navigation = () => {
   return (
     <div className="relative mx-auto">
       <div className="fixed top-0 left-0 right-0 flex justify-center items-center w-full h-16 bg-blue-500 z-50">
-        <div className="flex w-full lg:w-1/3 mx-4 lg:mx-0 justify-between">
+        <div className="flex w-full lg:w-1/3 mx-4 lg:mx-0 items-center space-x-2">
+          <button
+            onClick={() => {
+              router.back();
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="white"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
+          </button>
+
           <Link href={"/"}>
             <h1 className="text-white text-xl font-bold flex-grow-1">
               Poke Monster!
