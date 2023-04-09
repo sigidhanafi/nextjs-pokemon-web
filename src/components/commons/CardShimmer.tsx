04b6141b827ffd1d33d2c3be5f5a54b759682096
sigidhanafi <sigidhanafi@gmail.com>
2023-04-09 13:11:50 +0700

    fix shimmering responsive

--- src/components/commons/CardShimmer.tsx
+++ src/components/commons/CardShimmer.tsx
@@ -4,7 +4,7 @@ const CardShimmer = () => {
     shimmers.push(
       <div className="w-1/3 flex-shrink-0" key={i}>
         <div className="mx-2 my-2">
-          <div className="w-full h-52 bg-gray-200 rounded-lg"></div>
+          <div className="w-full h-36 lg:40 xl:h-52 bg-gray-200 rounded-lg"></div>
         </div>
       </div>
     );
--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -15,7 +15,7 @@ const PokemonCard = (props: PorkemonCardProps) => {
     <div className="relative w-1/3 flex-shrink-0">
       <Link href={"/" + props.name}>
         <div className="bg-blue-200 border border-blue-300 rounded-lg mx-2 my-2">
-          <Image src={props.image} width={300} height={500} alt="" />
+          <Image src={props.image} width={300} height={300} alt="" />
           <div className="flex justify-center p-2">
             <span className="text-blue-500 font-medium text-sm md:text-xl">
               {props.name}
